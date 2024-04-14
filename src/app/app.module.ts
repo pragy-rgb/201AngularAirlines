f51c3c802c98f41266ef39b213a4ef2c82a109ca
Pragya Kumari <p8590907@gmail.com>
2024-04-14 17:57:06 +0530

    Revert "commiting with the latest change"
    This reverts commit 9cbee12c20686b5b9aef1202194fd62301ed9f05.
    
    reverting because of problem with modules
    ^X

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,1 @@
 
-import { NgModule } from '@angular/core';
-import { CheckInComponent } from './check-in/check-in.component';
-import { HomeComponent } from './home/home.component';
-import { AppComponent } from './app.component';
-
-
-@NgModule({
-  imports: [
-  
-  ],
-  declarations: [CheckInComponent, HomeComponent, AppComponent]
-})
-export class AppModule { }
--- src/app/check-in/check-in.component.ts
+++ src/app/check-in/check-in.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-check-in',
+  standalone: true,
+  imports: [],
   templateUrl: './check-in.component.html',
   styleUrl: './check-in.component.css'
 })
