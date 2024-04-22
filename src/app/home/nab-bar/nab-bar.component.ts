69d18a22ed7573d9d1f9a14bd7c62601d27ee046
Pragya Kumari <p8590907@gmail.com>
2024-04-22 11:44:25 +0530

    Right structure

---
+++ src/app/home/nab-bar/nab-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nab-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nab-bar.component.html',
+  styleUrl: './nab-bar.component.css'
+})
+export class NabBarComponent {
+
+}
