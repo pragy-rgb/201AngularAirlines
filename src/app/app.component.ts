aaa7c4fc47ebf1260eb5a8cc1507f27834918311
Pragya Kumari <p8590907@gmail.com>
2024-04-27 20:43:29 +0530

    Did routing successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterOutlet, RouterLink]
 })
 export class AppComponent {
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NabBarComponent } from './home/nab-bar/nab-bar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"Home", title:"Home", component: HomeComponent},
+    {path:"Home/nav", title:"Home", component: NabBarComponent}
+    ];
